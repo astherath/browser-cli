b9835efd3223b8f5f81da573507e6f3ba53670e0
astherath <farceriv@gmail.com>
2021-04-07 20:53:03 -0400

    added build script

--- src/cli.rs
+++ src/cli.rs
@@ -67,9 +67,9 @@ impl SpecialUrl {
 
     fn to_url_string(self) -> String {
         match self {
-            Self::Github => "https://github.com/astherath",
+            Self::Github => "https://github.com/",
             Self::Gmail => "https://gmail.com",
-            Self::Logs => "https://dashboard.heroku.com/apps/sparkdev-underline/logs",
+            Self::Logs => "https://dashboard.heroku.com/",
             Self::Default => "https://google.com",
         }
         .to_string()
